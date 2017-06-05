ebbee72d61e5b028187e2d176d8ceea4e1061e85
nonameentername <nonameentername@gmail.com>
2017-06-04 19:08:40 -0500

    initial commit

---
+++ content/foo.md
@@ -0,0 +1,7 @@
+title: Everyday
+Date: 2017-10-05 18:27
+Category: Art
+Tags: Everyday, Art, Drawing
+picture: images/pic01.jpg
+minipicture: images/pic04.jpg
+tinypicture: images/pic08.jpg
---
+++ pelicanconf.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'werner m'
+SITENAME = u'werner mendizabal'
+SITEURL = 'http://localhost:8000'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+#DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 3
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+THEME = 'html5up-future-imperfect'
+FEED_ALL_RSS = 'rss'
+
+DEFAULT_DATE_FORMAT = ('%B %d, %Y')
+
+#DISQUS_SITENAME = 'tecunhuman'
+
+SITE_SUMMMARY = 'Artist, Musician, Programmer and Game Developer'
+
+ABOUT_TEXT = SITE_SUMMMARY
+#ABOUT_LINK = 'http://google.com'
+
+TWITTER_USER = 'nonameentername'
+FACEBOOK_USER = 'werner.io'
+INSTAGRAM_USER = 'nonameentername'
+MAIL = 'nonameentername@gmail.com'
